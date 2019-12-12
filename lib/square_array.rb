7be07fd596d175c66bcec4f8d2f1736e40e29402
Linda Ramos <ltramos7@gmail.com>
2019-12-12 20:55:51 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,39 @@
-def square_array(array)
-  # your code here
+
+#this works! partially
+# numbers = [1,2,3]
+# def square_array(numbers_array)
+#   counter = 0
+#   while numbers_array[counter] do
+#     puts numbers_array[counter]*numbers_array[counter]
+#     # puts (numbers[counter])**2
+#     counter += 1
+#   end
+# end
+
+# square_array(numbers)
+
+numbers = [1,2,3]
+def square_array(Array)
+  counter = 0
+  while Array[counter] do
+   # puts numbers_array[counter]*numbers_array[counter]
+   puts (Array[counter])**2
+    counter += 1
+  end
 end
+
+square_array(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
